e396c616d72ca85c039311422d3d0e6d051694d1
ShalomV <shalomvanunu@gmail.com>
2025-09-04 14:54:37 +0300

    Update 4/9

---
+++ Python/ClassExamples/Example2.py
@@ -0,0 +1,9 @@
+
+num1 = 3
+num2 = 4
+name =  ' shalom'
+last_name = 'vanunu'
+
+print(f'my name is {name} my family name is {last_name} years {num1}')
+
+print(num1*name)
---
+++ Python/ClassExamples/Example3.py
@@ -0,0 +1,9 @@
+
+name = input('Enter your name: ')
+
+print(name)
+
+num = int(input('Enter your num: '))
+print(num*name)
+
+
