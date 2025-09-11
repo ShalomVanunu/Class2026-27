4a8b72ce1b0cf67848e65e0fb6411b63cf8df9ca
ShalomV <shalomvanunu@gmail.com>
2025-09-11 15:34:40 +0300

    Update 11/9

---
+++ Python/ClassExamples/Example7.py
@@ -0,0 +1,8 @@
+
+targil = input(" Enter Calc :")
+
+if '+' in targil:
+    numbers = targil.split('+')
+    print(int(numbers[0])+int(numbers[1]))
+
+ 
---
+++ Python/ClassExamples/StructurePython.py
@@ -0,0 +1,20 @@
+
+# import modules
+import  random
+import string
+
+
+# Decalre Arg
+NAME = "SHalom"
+CLASS = " Yodalef5"
+
+#Function
+
+def names():
+    pass
+
+
+# Code
+print(NAME)
+
+
