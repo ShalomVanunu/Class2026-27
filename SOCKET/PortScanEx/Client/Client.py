4b529cb5ef1385755fcddac3662b9b4c701fa695
ShalomV <shalomvanunu@gmail.com>
2026-03-12 12:34:03 +0200

    Update 12/3

--- SOCKET/PortScanEx/Client/Client.py
+++ SOCKET/PortScanEx/Client/Client.py
@@ -1,8 +1,19 @@
 import PortScan
-
+import socket
 
 IP = "172.20.138.211"
+PORT = 5555
 
-print(PortScan.portscan(IP))
+Client_soc = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+Client_soc.connect((IP,PORT))
+print(" client Connected...")
+
+while True:
+    data = Client_soc.recv(1024).decode()
+    if data == "bye":
+        break
+    if data== "start":
+        list_ports = PortScan.portscan(IP)
+        print(list_ports)
 
 
---
+++ SOCKET/PortScanEx/Server/Server.py
@@ -0,0 +1,30 @@
+import socket
+
+
+
+IP = "172.20.138.211"
+PORT = 5555
+
+def main():
+    Server_Soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    Server_Soc.bind((IP,PORT))
+    Server_Soc.listen()
+    print(" Server Waiting Client to connect...")
+    print("Check")
+    Client_soc, IP_Port = Server_Soc.accept()
+    print(f" client connected ...{IP_Port}")
+
+    while True:
+        choose = input(""" Choose :
+         1 . Start Scan
+         2. Exit""")
+        if int(choose) == 2:
+            Client_soc.send("bye".encode())
+            print("Bye")
+            break
+        Client_soc.send("start".encode())
+
+
+
+
+main()
